4cfc6bcd2c60a7c3fdbe06ff138700eea3d489e9
Matheus Mutti <matheusmuttiteixeira@gmail.com>
2023-10-17 16:23:29 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+
+Projeto para exemplificar o uso de orientação a objeto
+
+Encapsulamento:
+
+    Expor somente o necessário através de interfaces definidas.
+        Atributo ser privado, mas ele ser acessado através de métodos.
+
+Henrança:
+
+    Criar nova classe baseada em uma classe existente.
+        Cachorro : Animal
+
+Polimorfismo:
+
+    Objetos de classes diferentes sejam tratados como objetos da mesma classe base.
+
+Abstração:
+
+    Modelar objetos do mundo real como classes e objetos no código.
+        A classe veículo tem objetos e métodos, porém para criar uma instância precisa de mais informações
+        que são definnidar a partir de outras classes.
+
+*/ 
+
